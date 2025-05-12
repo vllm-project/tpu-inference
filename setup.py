7668acd9795deef26686ffc55959f19273d57808
Andrew Vuong <akvuong@google.com>
2025-05-12 17:06:22 +0000

    Add Dockerfile to build vLLM and tpu_commons.
    Signed-off-by: Andrew Vuong <akvuong@google.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: Apache-2.0
+import os
+from typing import List
 
 from setuptools import find_packages, setup
+
+ROOT_DIR = os.path.dirname(__file__)
+
+
+def get_path(*filepath) -> str:
+    return os.path.join(ROOT_DIR, *filepath)
+
+
+def get_requirements() -> List[str]:
+    """Get Python package dependencies from requirements.txt."""
+
+    def _read_requirements(filename: str) -> List[str]:
+        with open(get_path(filename)) as f:
+            requirements = f.read().strip().split("\n")
+        resolved_requirements = []
+        for line in requirements:
+            if line.startswith("-r "):
+                resolved_requirements += _read_requirements(line.split()[1])
+            elif line.startswith("--"):
+                continue
+            else:
+                resolved_requirements.append(line)
+        return resolved_requirements
+
+    try:
+        requirements = _read_requirements("requirements.txt")
+    except ValueError:
+        print("Failed to read requirements.txt in vllm_tpu.")
+    return requirements
+
 
 setup(
     name="tpu_commons",
@@ -13,11 +45,7 @@ setup(
     author="tpu_commons Contributors",
     packages=find_packages(),
     python_requires=">=3.10",
-    install_requires=[
-        "torch>=2.0.0",
-        "torchxla>=2.0.0",
-        "torchvision>=0.15.0",
-    ],
+    install_requires=get_requirements(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
