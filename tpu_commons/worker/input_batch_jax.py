ef1dd5de9a1df3169cc7c2e2feb283a685b7e6f5
Pooya Moradi <pooyam@google.com>
2025-06-03 01:45:50 +0000

    Set default top_k to 1.
    Signed-off-by: Pooya Moradi <pooyam@google.com>

--- tpu_commons/worker/input_batch_jax.py
+++ tpu_commons/worker/input_batch_jax.py
@@ -255,7 +255,7 @@ class InputBatch:
         if 0 < top_k < self.vocab_size:
             self.top_k_reqs.add(req_id)
         else:
-            top_k = self.vocab_size
+            top_k = 1
         self.top_k_cpu[req_index] = top_k
         self.min_p_cpu[req_index] = sampling_params.min_p
         self.frequency_penalties_cpu[
