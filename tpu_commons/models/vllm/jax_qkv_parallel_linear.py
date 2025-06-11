8fc008230568b8a9da97aab858971d425f5b705a
hfan <fanhongmin@google.com>
2025-06-10 22:21:51 -0400

    Multi-chip support in running vLLM models via torchax + SPMD. (#33)
    Signed-off-by: Hongmin Fan <fanhongmin@google.com>

---
+++ tpu_commons/models/vllm/jax_qkv_parallel_linear.py
@@ -0,0 +1,100 @@
+from typing import Optional
+
+import jax
+import torch
+import torch.nn.functional as F
+from jax.sharding import Mesh, NamedSharding, PartitionSpec
+from torch.nn.parameter import Parameter
+from torchax.interop import torch_view
+from torchax.tensor import t2j
+from vllm.model_executor.layers.linear import QKVParallelLinear
+
+P = PartitionSpec
+
+
+class JaxQKVParallelLinear(torch.nn.Module):
+
+    def __init__(self, qkv_linear: torch.nn.Module, mesh: Mesh):
+        super().__init__()
+        assert isinstance(qkv_linear, QKVParallelLinear)
+        self.skip_bias_add = qkv_linear.skip_bias_add
+        self.return_bias = qkv_linear.return_bias
+
+        self.q_weight: Parameter
+        self.k_weight: Parameter
+        self.v_weight: Parameter
+        self.q_bias: Optional[Parameter]
+        self.k_bias: Optional[Parameter]
+        self.v_bias: Optional[Parameter]
+        self._load_weights_from_qkv_linear(qkv_linear)
+        self._shard_weight(mesh)
+
+    def _shard_weight(self, mesh: Mesh):
+        self.q_weight.apply_jax_(jax.device_put,
+                                 NamedSharding(mesh, P('model', None)))
+        self.k_weight.apply_jax_(jax.device_put,
+                                 NamedSharding(mesh, P('model', None)))
+        self.v_weight.apply_jax_(jax.device_put,
+                                 NamedSharding(mesh, P('model', None)))
+
+        if self.q_bias is not None:
+            assert self.k_bias is not None and self.v_bias is not None, \
+                "QKVParallelLinear should have q, k, and v biases together."
+            self.q_bias.apply_jax_(jax.device_put,
+                                   NamedSharding(mesh, P('model')))
+            self.k_bias.apply_jax_(jax.device_put,
+                                   NamedSharding(mesh, P('model')))
+            self.v_bias.apply_jax_(jax.device_put,
+                                   NamedSharding(mesh, P('model')))
+
+    def _load_weights_from_qkv_linear(self, qkv_linear: torch.nn.Module):
+        q_proj_size, k_proj_size, _ = qkv_linear.output_sizes
+        # The weight of qkv linear is a concatenation of q, k, and v weights
+        # along the output dimension.
+        qkv_weight = torch_view(t2j(qkv_linear.weight.data))
+        q_weight = Parameter(qkv_weight[:q_proj_size], requires_grad=False)
+        k_weight = Parameter(qkv_weight[q_proj_size:q_proj_size + k_proj_size],
+                             requires_grad=False)
+        v_weight = Parameter(qkv_weight[q_proj_size + k_proj_size:],
+                             requires_grad=False)
+        self.register_parameter("q_weight", q_weight)
+        self.register_parameter("k_weight", k_weight)
+        self.register_parameter("v_weight", v_weight)
+
+        if qkv_linear.bias is not None:
+            bias = torch_view(t2j(qkv_linear.bias))
+            q_bias = Parameter(bias[:q_proj_size], requires_grad=False)
+            k_bias = Parameter(bias[q_proj_size:q_proj_size + k_proj_size],
+                               requires_grad=False)
+            v_bias = Parameter(bias[q_proj_size + k_proj_size:],
+                               requires_grad=False)
+            self.register_parameter("q_bias", q_bias)
+            self.register_parameter("k_bias", k_bias)
+            self.register_parameter("v_bias", v_bias)
+        else:
+            self.register_parameter("q_bias", None)
+            self.register_parameter("k_bias", None)
+            self.register_parameter("v_bias", None)
+
+    def forward(self, input):
+        # Same forward functionality as QKVParallelLinear, but doing qkv porj
+        # separately.
+        q_bias = self.q_bias if not self.skip_bias_add else None
+        k_bias = self.k_bias if not self.skip_bias_add else None
+        v_bias = self.v_bias if not self.skip_bias_add else None
+        q_proj = F.linear(input, self.q_weight, q_bias)
+        k_proj = F.linear(input, self.k_weight, k_bias)
+        v_proj = F.linear(input, self.v_weight, v_bias)
+        # The q/k/v projections will be split outside of the QKVParallelLinear.
+        # Because we are replacing XlaQKVParallelLinear with the
+        # QKVParallelLinear, we need to concatenate q, k, and v projections to
+        # match the output shape of the QKVParallelLinear implementation even if
+        # it seems to be redundant.
+        # The concat and the following split will be noop, and should be
+        # optimized away by the compiler.
+        qkv_proj = torch.cat([q_proj, k_proj, v_proj], dim=-1)
+        output_bias = torch.cat([q_bias, k_bias, v_bias], dim=-1) if \
+                            self.skip_bias_add else None
+        if not self.return_bias:
+            return qkv_proj
+        return qkv_proj, output_bias
---
+++ tpu_commons/models/vllm/sharding.py
@@ -0,0 +1,111 @@
+import jax
+import torch
+import torchax
+from jax.sharding import Mesh, NamedSharding, PartitionSpec
+from torch.nn.parameter import Parameter
+from torch.utils import _pytree as pytree
+from torchax.interop import extract_all_buffers, torch_view
+from torchax.tensor import t2j
+from vllm.attention import Attention as VllmAttention
+from vllm.logger import init_logger
+from vllm.model_executor.layers.linear import (ColumnParallelLinear,
+                                               QKVParallelLinear,
+                                               RowParallelLinear)
+
+from tpu_commons.models.vllm.jax_attention import JaxAttention
+from tpu_commons.models.vllm.jax_qkv_parallel_linear import \
+    JaxQKVParallelLinear
+
+P = PartitionSpec
+
+logger = init_logger(__name__)
+
+
+def shard_attention(layer: torch.nn.Module, mesh: Mesh):
+    return JaxAttention(layer, mesh)
+
+
+def shard_qkv_parallel_linear(layer: torch.nn.Module, mesh: Mesh):
+    assert isinstance(layer, QKVParallelLinear)
+    jax_layer = JaxQKVParallelLinear(layer, mesh)
+    return jax_layer
+
+
+def shard_column_parallel_linear(layer: torch.nn.Module, mesh: Mesh):
+    assert isinstance(layer, ColumnParallelLinear)
+    w = Parameter(torch_view(t2j(layer.weight)))
+    layer.weight = w.apply_jax_(jax.device_put,
+                                NamedSharding(mesh, P('model', None)))
+    return layer
+
+
+def shard_row_parallel_linear(layer: torch.nn.Module, mesh: Mesh):
+    assert isinstance(layer, RowParallelLinear)
+    w = Parameter(torch_view(t2j(layer.weight)))
+    layer.weight = w.apply_jax_(jax.device_put,
+                                NamedSharding(mesh, P(None, 'model')))
+    return layer
+
+
+MODULE_TYPE_TO_WRAPPING_FUNC = {
+    VllmAttention: shard_attention,
+    QKVParallelLinear: shard_qkv_parallel_linear,
+    ColumnParallelLinear: shard_column_parallel_linear,
+    RowParallelLinear: shard_row_parallel_linear,
+}
+
+
+def shard_parallel_layers_to_tpu(model: torch.nn.Module, mesh: Mesh) -> None:
+
+    def _shard_layer(module, name=None, parent=None):
+        for module_type, wrapping_func in MODULE_TYPE_TO_WRAPPING_FUNC.items():
+            if isinstance(module, module_type):
+                wrapped_module = wrapping_func(module, mesh)
+
+                assert parent is not None and name is not None, (
+                    "Top Level module is not expected to be wrapped.")
+                logger.debug("replace %s with %s", module, wrapped_module)
+                setattr(parent, name, wrapped_module)
+
+                module = wrapped_module
+                break
+
+        for child_name, child_module in list(module.named_children()):
+            _shard_layer(child_module, child_name, module)
+
+    _shard_layer(model)
+
+
+def shard_model_to_tpu(model: torch.nn.Module, mesh: Mesh):
+    """
+    Shard the model weights and move them to TPU.
+
+    At the same time, also turn the weight tensors into torchax tensors so that
+    jax code can interop with it and the overall program can be traced and
+    compiled in XLA.
+
+    Args:
+        model: A PyTorch model whose weights are on CPU main memory.
+        mesh: JAX mesh object for sharding.
+    """
+
+    def _is_unmoved_tensor(x):
+        # tensors haven't been turned into torchax tensor are the ones not moved to TPU yet.
+        return isinstance(
+            x, torch.Tensor) and not isinstance(x, torchax.tensor.Tensor)
+
+    def _move_to_tpu_replicated(x):
+        return torch_view(t2j(x)).apply_jax_(jax.device_put,
+                                             NamedSharding(mesh, P()))
+
+    with jax.default_device(jax.devices("cpu")[0]), torchax.default_env():
+        shard_parallel_layers_to_tpu(model, mesh)
+
+        # For other weight tensors, repliate them on all the TPU chips.
+        params, buffers = extract_all_buffers(model)
+        params, buffers = pytree.tree_map_only(_is_unmoved_tensor,
+                                               _move_to_tpu_replicated,
+                                               (params, buffers))
+        params_and_buffers = {**params, **buffers}
+
+        return params_and_buffers
