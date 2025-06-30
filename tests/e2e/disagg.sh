08d3adf2d3aa0ccdcf90fbc1bb73136877f7b9e7
Hongmin Fan <fanhongmin@google.com>
2025-06-30 15:56:17 +0000

    Add logs, improve disagg e2e test script.
    Signed-off-by: Hongmin Fan <fanhongmin@google.com>

--- tests/e2e/disagg.sh
+++ tests/e2e/disagg.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+    echo "Error: need to specify the local path to the root directory of vllm repo." >&2
+    exit 1
+fi
+
+VLLM_CODE_PATH=$1
+
 # This script appends specific import and assignment lines to the vLLM engine's core.py file.
-FILE_PATH="$HOME/vllm/vllm/v1/engine/core.py"
+FILE_PATH="$VLLM_CODE_PATH/vllm/v1/engine/core.py"
 CONTENT_TO_ADD="
 # Added for TPU support
 from tpu_commons.core.core_tpu import EngineCore as TPUEngineCore
@@ -16,9 +23,7 @@ if [ -f "$FILE_PATH" ]; then
     echo "$CONTENT_TO_ADD" >> "$FILE_PATH"
 fi
 
-cd $HOME
-
-TPU_BACKEND_TYPE=jax python vllm/examples/offline_inference/basic/generate.py --task=generate --model=meta-llama/Meta-Llama-3-8B-Instruct --max_model_len=1024 --max_num_seqs=8
+TPU_BACKEND_TYPE=jax python "$VLLM_CODE_PATH/examples/offline_inference/basic/generate.py" --task=generate --model=meta-llama/Meta-Llama-3-8B-Instruct --max_model_len=1024 --max_num_seqs=8
 
 head -n -8 "$FILE_PATH" > "tmp.py"
 mv "tmp.py" "$FILE_PATH"
