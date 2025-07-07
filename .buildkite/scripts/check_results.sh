a0dc560be4686be5412eaa8601dbf9c9b42fd0b2
wwl2755-google <wenlongwang@google.com>
2025-07-07 11:47:32 -0700

    [CI] fix the bugs where CI failure cannot be captured (#134)
    Signed-off-by: wwl2755-google <wenlongwang@google.com>

--- .buildkite/scripts/check_results.sh
+++ .buildkite/scripts/check_results.sh
@@ -2,27 +2,32 @@
 set -e
 
 ANY_FAILED=false
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <failure_label> <step_key_1> <step_key_2> ..."
+    exit 1
+fi
 
-STEP_KEYS="tpu_test_0 tpu_test_1 tpu_test_2 tpu_test_3 tpu_test_4 tpu_test_5 tpu_test_6 tpu_test_7 tpu_test_8 tpu_test_9 tpu_test_10 tpu_test_11 tpu_test_12 tpu_test_13"
+FAILURE_LABEL="$1"
+shift
 
 echo "--- Checking Test Outcomes"
 
-for KEY in $STEP_KEYS; do
+for KEY in "$@"; do
     OUTCOME=$(buildkite-agent step get "outcome" --step "${KEY}" || echo "skipped")
     echo "Step ${KEY} outcome: ${OUTCOME}"
 
     if [ "${OUTCOME}" != "passed" ] && [ "${OUTCOME}" != "skipped" ] ; then
-    ANY_FAILED=true
+        ANY_FAILED=true
     fi
 done
 
 if [ "${ANY_FAILED}" = "true" ] ; then
     cat <<- YAML | buildkite-agent pipeline upload
     steps:
-    - label: "TPU V1 Test failed"
+    - label: "${FAILURE_LABEL}"
         agents:
         queue: tpu_v6_queue
-        command: echo "TPU V1 Test failed"
+        command: echo "${FAILURE_LABEL}"
 YAML
     exit 1
 else
