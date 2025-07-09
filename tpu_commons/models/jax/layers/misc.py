bb217bea861cbd5358d4cc12a84019ccff41548d
Xiang Xu <xiangxu@google.com>
2025-07-09 04:15:31 +0000

    Fix CI
    Signed-off-by: Xiang Xu <xiangxu@google.com>

--- tpu_commons/models/jax/layers/misc.py
+++ tpu_commons/models/jax/layers/misc.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 
 import jax
@@ -8,4 +9,8 @@ from jax.sharding import PartitionSpec as P
 # TODO(xiang): move this to weight_utils.py
 def shard_put(x: jax.Array, sharding_names: Tuple[str, ...] | P,
               mesh: jax.sharding.Mesh) -> jax.Array:
+    # Single device sharding requires this special handling
+    # to avoid the recursive jit error.
+    if math.prod(mesh.axis_sizes) == 1:
+        return jax.device_put(x, mesh.devices.tolist()[0][0])
     return jax.device_put(x, NamedSharding(mesh, P(*sharding_names)))
