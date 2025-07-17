7540bfca7eb576347864a2ecc3f96428bf92a34f
bzgoogle <beinuoz@google.com>
2025-07-17 00:00:39 +0000

    To support Llama3 70B model on new model implementation; To achieve accuracy & perf parity between new and the existing one
    Signed-off-by: bzgoogle <beinuoz@google.com>

--- tpu_commons/models/jax/model_loader.py
+++ tpu_commons/models/jax/model_loader.py
@@ -24,10 +24,14 @@ def _get_model_architecture(config: PretrainedConfig) -> nnx.Module:
     _MODEL_REGISTRY["Qwen2ForCausalLM"] = Qwen2ForCausalLM
 
     if os.getenv("NEW_MODEL_DESIGN", False):
+<<<<<<< HEAD
         from tpu_commons.models.jax.recipes.llama3 import Llama3_8B
+=======
+        from tpu_commons.models.jax.recipes.llama3 import Llama3
+>>>>>>> dd54e92 (To support Llama3 70B model on new model implementation; To achieve accuracy & perf parity between new and the existing one)
 
         # from tpu_commons.models.jax.recipes.llama4 import Llama4Scout
-        _MODEL_REGISTRY["Llama3_8B"] = Llama3_8B
+        _MODEL_REGISTRY["Llama3"] = Llama3
         # _MODEL_REGISTRY["Llama4Scout"] = Llama4Scout
 
     architectures = getattr(config, "architectures", [])
