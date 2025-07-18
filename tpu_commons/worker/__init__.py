4c2bd9a01c9b16fc3fb743f3c15745b11c677aef
Siyuan Liu <lsiyuan@google.com>
2025-07-18 21:10:34 +0000

    make jax the default backend
    Signed-off-by: Siyuan Liu <lsiyuan@google.com>
    (cherry picked from commit 501486c311f3a381728d0a340db48eda174a22a9)

--- tpu_commons/worker/__init__.py
+++ tpu_commons/worker/__init__.py
@@ -10,23 +10,20 @@ def get_tpu_worker_cls(worker_type=None):
 
     # Use environment variable if no explicit type is provided
     if worker_type is None:
-        worker_type = os.environ.get("TPU_BACKEND_TYPE", "pytorch_xla").lower()
+        worker_type = os.environ.get("TPU_BACKEND_TYPE", "jax").lower()
 
-    if worker_type == "pytorch_xla":
-        from tpu_commons.worker.tpu_torch_xla_worker import TPUWorker
+    if worker_type == "jax":
+        from tpu_commons.worker.tpu_worker_jax import TPUWorker
         return TPUWorker
     elif worker_type == "torchax":
         from tpu_commons.worker.tpu_worker_torchax import TPUWorker
-        return TPUWorker
-    elif worker_type == "jax":
-        from tpu_commons.worker.tpu_worker_jax import TPUWorker
         return TPUWorker
     else:
         raise ValueError(f"Unknown TPU worker type: {worker_type}")
 
 
 # For convenience, also export the default worker
-TPU_BACKEND_TYPE = os.environ.get("TPU_BACKEND_TYPE", "pytorch_xla").lower()
+TPU_BACKEND_TYPE = os.environ.get("TPU_BACKEND_TYPE", "jax").lower()
 try:
     TPUWorker = get_tpu_worker_cls(TPU_BACKEND_TYPE)
     __all__.append("TPUWorker")
