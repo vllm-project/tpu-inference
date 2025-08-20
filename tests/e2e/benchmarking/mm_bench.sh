ab81dfc2d09fe75f266d841419f79be0820df868
wwl2755-google <wenlongwang@google.com>
2025-08-20 11:02:56 -0700

    fix
    Signed-off-by: wwl2755-google <wenlongwang@google.com>

--- tests/e2e/benchmarking/mm_bench.sh
+++ tests/e2e/benchmarking/mm_bench.sh
@@ -8,3 +8,13 @@ vllm bench throughput \
   --num-prompts 10 \
   --hf-split train \
   --max_num_batched_tokens 98304
+
+
+vllm bench serve \
+  --model Qwen/Qwen2.5-VL-3B-Instruct \
+  --backend vllm-chat \
+  --dataset-name hf \
+  --dataset-path lmarena-ai/VisionArena-Chat \
+  --num-prompts 10 \
+  --hf-split train \
+  --max_num_batched_tokens 98304
