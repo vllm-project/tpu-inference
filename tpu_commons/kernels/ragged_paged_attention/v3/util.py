abd6bed0ea6da8f89beeba5d58cf075c031f25a1
Jevin Jiang <jevin0change@gmail.com>
2025-09-10 14:04:32 -0700

    [RPA3] Fix get_device_name
    Signed-off-by: Jevin Jiang <jevin0change@gmail.com>

--- tpu_commons/kernels/ragged_paged_attention/v3/util.py
+++ tpu_commons/kernels/ragged_paged_attention/v3/util.py
@@ -49,11 +49,14 @@ def get_device_name(num_devices: int | None = None):
     kind = jax.devices()[0].device_kind
     if 'TPU' not in kind:
         raise RuntimeError('Expected TPU devices')
+    suffix = ''
     if kind.endswith(' lite'):
-        kind = kind[:-len(' lite')] + 'e'
+        kind = kind[:-len(' lite')]
+        suffix = 'e'
     if kind == 'TPU7x':
         kind = 'TPU v7'
     assert kind[:-1] == 'TPU v', kind
+    kind += suffix
     if num_devices is not None:
         kind += f'-{num_devices}'
     return kind
