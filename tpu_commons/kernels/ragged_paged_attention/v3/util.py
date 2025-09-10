bdfcb2b7120dd71f3da750860f2c9cfe15c5b56a
Jevin Jiang <jevin0change@gmail.com>
2025-09-09 23:43:18 -0700

    [RPA3] Updated tuned block sizes for tpu v7
    Signed-off-by: Jevin Jiang <jevin0change@gmail.com>

--- tpu_commons/kernels/ragged_paged_attention/v3/util.py
+++ tpu_commons/kernels/ragged_paged_attention/v3/util.py
@@ -1,7 +1,4 @@
 """Utility functions for ragged paged attention."""
-
-import re
-
 import jax
 from jax._src import dtypes
 
@@ -38,14 +35,25 @@ def next_power_of_2(x: int):
 def get_tpu_version() -> int:
     """Returns the numeric version of the TPU, or -1 if not on TPU."""
     kind = jax.devices()[0].device_kind
-    match = re.match(r'^TPU[^\d]*(\d+)', kind)
-    if match is None:
+    if 'TPU' not in kind:
         return -1
-    return int(match.group(1))
+    if kind.endswith(' lite'):
+        kind = kind[:-len(' lite')]
+    if kind == 'TPU7x':
+        return 7
+    assert kind[:-1] == 'TPU v', kind
+    return int(kind[-1])
 
 
 def get_device_name(num_devices: int | None = None):
-    name = ' '.join(jax.devices()[0].device_kind.split()[:2])
+    kind = jax.devices()[0].device_kind
+    if 'TPU' not in kind:
+        raise RuntimeError('Expected TPU devices')
+    if kind.endswith(' lite'):
+        kind = kind[:-len(' lite')] + 'e'
+    if kind == 'TPU7x':
+        kind = 'TPU v7'
+    assert kind[:-1] == 'TPU v', kind
     if num_devices is not None:
-        name += f'-{num_devices}'
-    return name
+        kind += f'-{num_devices}'
+    return kind
