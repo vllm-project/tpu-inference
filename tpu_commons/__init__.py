416e45c99c35304d33473b2d2c7a68dcdf355b2b
wenxindongwork <wenxindong@google.com>
2025-09-14 21:49:01 +0000

    wip
    Signed-off-by: wenxindongwork <wenxindong@google.com>

--- tpu_commons/__init__.py
+++ tpu_commons/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from tpu_commons import tpu_info as ti
 from tpu_commons.logger import init_logger
+import jax 
 
 logger = init_logger(__name__)
 
@@ -13,18 +14,32 @@ PROD_WARNING = (
 
 logger.warn(PROD_WARNING)
 
-logger.info(f"TPU info: node_name={ti.get_node_name()} | "
-            f"tpu_type={ti.get_tpu_type()} | "
-            f"worker_id={ti.get_node_worker_id()} | "
-            f"num_chips={ti.get_num_chips()} | "
-            f"num_cores_per_chip={ti.get_num_cores_per_chip()}")
 
-# Must run pathwaysutils.initialize() before any JAX operations.
-if "proxy" in os.environ.get('JAX_PLATFORMS', '').lower():
+def detect_platform() -> str:
+    if "proxy" in os.environ.get('JAX_PLATFORMS', '').lower():
+        return "pathways"
+    return jax.lib.xla_bridge.get_backend().platform # 'tpu' or 'cpu'
+        
+_platform = detect_platform()
+
+if _platform == "tpu":
+    try:
+        logger.info("Running vLLM on TPU.")
+        logger.info(f"TPU info: node_name={ti.get_node_name()} | "
+                    f"tpu_type={ti.get_tpu_type()} | "
+                    f"worker_id={ti.get_node_worker_id()} | "
+                    f"num_chips={ti.get_num_chips()} | "
+                    f"num_cores_per_chip={ti.get_num_cores_per_chip()}")
+    except Exception as e:
+        logger.error(f"Error occurred while logging TPU info: {e}")
+elif _platform== "pathways":
+    logger.info("Running vLLM on TPU via Pathways proxy.")
     import pathwaysutils
     pathwaysutils.initialize()
     logger.info("Running vLLM with Pathways. "
                 "Module pathwaysutils is imported.")
+elif _platform == "cpu":
+    logger.info("Running vLLM on CPU.")
+
 else:
-    logger.info("Running vLLM without Pathways. "
-                "Module pathwaysutils is not imported.")
+    logger.error(f"Unsupported platform: {_platform}")
