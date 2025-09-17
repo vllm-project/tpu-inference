77bc147c63764e54060ea349e83950d0d29ad86d
Jevin Jiang <jevin0change@gmail.com>
2025-09-16 19:31:07 -0700

    Fix kv sharding for packed type (#704)
    Signed-off-by: Jevin Jiang <jevin0change@gmail.com>

--- tests/models/vllm/layers/test_pallas_torchax.py
+++ tests/models/vllm/layers/test_pallas_torchax.py
@@ -12,10 +12,10 @@ from vllm.attention.backends.abstract import AttentionType
 
 from tpu_commons.attention.backends.pallas_torchax import (
     PallasAttentionBackend, PallasAttentionBackendImpl)
-from tpu_commons.models.jax.attention import get_kv_cache_shape_with_mesh
 from tpu_commons.models.jax.attention_metadata import AttentionMetadata
 from tpu_commons.models.vllm.vllm_model_wrapper_context import \
     set_vllm_model_wrapper_context
+from tpu_commons.runner.kv_cache import get_kv_cache_shape_with_mesh
 
 # ---- Test Configuration & Constants ----
 
