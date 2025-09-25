13ff18c5205a944f9623fb897ccb42e463f964c5
XiongfeiWei <isaacwxf23@gmail.com>
2025-09-25 14:23:28 -0700

    Fix an CI issue due to the removal of _VLLM_V1 suffixes from attention backend names (#744)
    Signed-off-by: Xiongfei Wei <isaacwxf23@gmail.com>

--- tpu_commons/platforms/tpu_torchax.py
+++ tpu_commons/platforms/tpu_torchax.py
@@ -40,8 +40,7 @@ class TpuPlatform(Platform):
                              block_size: int, use_v1: bool,
                              use_mla: bool) -> str:
         assert use_v1, "only v1 is supported"
-        if (selected_backend != _Backend.PALLAS
-                and selected_backend != _Backend.PALLAS_VLLM_V1):
+        if selected_backend != _Backend.PALLA:
             logger.info("Cannot use %s backend on TPU.", selected_backend)
 
         return "tpu_commons.attention.backends.pallas_torchax.PallasAttentionBackend"
