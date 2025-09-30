34ade7af5643ca79d247d76dce92c4937cd7bf63
sixiang-google <sixiang-google>
2025-09-30 22:09:37 +0000

    change init handshaking of DisaggEngineCore to align to EngineCore
    Signed-off-by: sixiang-google <sixiang-google>

--- tpu_commons/core/disagg_executor.py
+++ tpu_commons/core/disagg_executor.py
@@ -11,7 +11,7 @@ from vllm.utils import (get_distributed_init_method, get_ip, get_open_port,
 from vllm.v1.executor.abstract import Executor
 from vllm.v1.executor.utils import get_and_update_mm_cache
 from vllm.v1.outputs import AsyncModelRunnerOutput
-from vllm.worker.worker_base import WorkerWrapperBase
+from vllm.v1.worker.worker_base import WorkerWrapperBase
 
 logger = init_logger(__name__)
 
