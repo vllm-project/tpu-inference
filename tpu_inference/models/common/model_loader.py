b397d8df2703aaeb4cab5ad983f68642b8c739eb
bzgoogle <beinuoz_google_com@t1v-n-3f245f84-w-0.us-central1-c.c.tpu-prod-env-one-vm.internal>
2025-10-03 21:54:38 +0000

    local change to support 2d TP for DeepSeek

--- tpu_inference/models/common/model_loader.py
+++ tpu_inference/models/common/model_loader.py
@@ -218,9 +218,7 @@ def get_flax_model(
             vllm_config.model_config.hf_config)
     jit_model = _get_nnx_model(model_class, vllm_config, rng, mesh)
     kv_cache_sharding = NamedSharding(
-        mesh,
-        PartitionSpec(ShardingAxisName.ATTN_DATA, None,
-                      ShardingAxisName.ATTN_HEAD))
+        mesh, PartitionSpec(ShardingAxisName.ATTN_DATA, None, (("no_attn_dp", "model"), "expert")))
     hidden_states_sharding = NamedSharding(mesh,
                                            PartitionSpec(
                                                ShardingAxisName.ATTN_DATA,
@@ -249,6 +247,7 @@ def get_flax_model(
     logits_sharding = NamedSharding(
         mesh,
         PartitionSpec(ShardingAxisName.MLP_DATA, ShardingAxisName.MLP_TENSOR))
+
 
     @functools.partial(
         jax.jit,
