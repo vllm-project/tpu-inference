a4f5b47f7810da67e2627d5795c34f71d30f45ca
Hongmin Fan <fanhongmin@google.com>
2025-10-06 16:01:57 +0000

    Catch up with vLLM upstream change.
    Signed-off-by: Hongmin Fan <fanhongmin@google.com>

--- tpu_commons/runner/multimodal_manager.py
+++ tpu_commons/runner/multimodal_manager.py
@@ -98,7 +98,7 @@ class MultiModalManager:
         # encoder outputs.
         encoder_outputs = []
         for _, num_items, mm_kwargs_group in group_mm_kwargs_by_modality(
-                mm_kwargs):
+                mm_kwargs, merge_by_field_config=False):
             batched_mm_inputs = mm_kwargs_group
             # Convert torch tensors to numpy arrays that JAX can handle.
             if "pixel_values" in batched_mm_inputs and isinstance(
