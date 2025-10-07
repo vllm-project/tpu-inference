9f364e09638e19c8643b63a323ee89ddad969143
dennis yeh <dennis.yeh@cienet.com>
2025-10-07 14:50:14 +0800

    accomplish integration test, this change adds a new test accuracy on TPU to the CI pipeline.
    Signed-off-by: StingLin <sting.lin@cienet.com>
    
    Signed-off-by: dennis yeh <dennis.yeh@cienet.com>

---
+++ scripts/vllm/integration/conftest.py
@@ -0,0 +1,25 @@
+def pytest_addoption(parser):
+    """Adds custom command-line options to pytest."""
+    parser.addoption(
+        "--tensor-parallel-size",
+        type=int,
+        default=1,
+        help="The tensor parallel size to use for the test.")
+    parser.addoption(
+        "--expected-value",
+        type=float,
+        default=None,
+        help="This value will be used to compare the measure value and determine if the test passes or fails.")
+    parser.addoption(
+        "--model-name",
+        type=str,
+        default=None,
+        help=
+        "Model name to test (e.g., 'model1')")
+    parser.addoption(
+        "--fp8-kv-model-name",
+        type=str,
+        default=None,
+        help=
+        "Model name to test fp8-kv (e.g., 'model1')"
+    )
