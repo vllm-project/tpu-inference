fb5ff2b16f6ab79379d694e7a6dd594d49ab5fea
wenxindongwork <wenxindong@google.com>
2025-10-15 19:37:20 +0000

    rebase

--- tpu_inference/runner/tpu_jax_runner.py
+++ tpu_inference/runner/tpu_jax_runner.py
@@ -283,16 +283,7 @@ class TPUModelRunner(KVConnectorModelRunnerMixin, LoRAModelRunnerMixin):
                                             dtype=np.int64)
 
     def load_model(self):
-        (
-            self.model_fn,
-            self.compute_logits_fn,
-            self.combine_hidden_states_fn,
-            self.get_multimodal_embeddings_fn,
-            self.get_input_embeddings_fn,
-            self.state,
-            self.lora_manager,
-            self.model,
-        ) = get_model(
+        self.model_fn, self.compute_logits_fn, self.combine_hidden_states_fn, self.get_multimodal_embeddings_fn, self.get_input_embeddings_fn, self.get_mrope_input_positions_fn, self.state, self.lora_manager, self.model = get_model(
             self.vllm_config,
             self.rng_key,
             self.mesh,
