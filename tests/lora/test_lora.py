75bf0eb988f212510d9a63ed873ad7c1678a0098
Xiongfei Wei <isaacwxf23@gmail.com>
2025-10-21 04:22:11 +0000

    only run multi-lora test
    Signed-off-by: Xiongfei Wei <isaacwxf23@gmail.com>

--- tests/lora/test_lora.py
+++ tests/lora/test_lora.py
@@ -1,6 +1,5 @@
 # https://github.com/vllm-project/vllm/blob/ed10f3cea199a7a1f3532fbe367f5c5479a6cae9/tests/tpu/lora/test_lora.py
 import os
-import subprocess
 
 import pytest
 import vllm
@@ -37,20 +36,6 @@ def setup_vllm(num_loras: int, tp: int = 1) -> vllm.LLM:
                     enable_lora=True,
                     max_loras=num_loras,
                     max_lora_rank=8)
-
-
-@pytest.fixture(autouse=True)
-def run_after_each_test():
-    # --- Setup code (runs before each test) ---
-    # print("\nSetting up...")
-    yield  # This is where the test runs
-    # --- Teardown code (runs after each test) ---
-    command = "lsof -t /dev/vfio/* | xargs kill"
-    results = subprocess.run(command,
-                             shell=True,
-                             capture_output=True,
-                             text=True)
-    print(f"Killing TPU resources: {results.stdout}, {results.stderr}")
 
 
 # For multi-chip test, we only use TP=2 because the base model Qwen/Qwen2.5-3B-Instruct has 2 kv heads and the current attention kernel requires it to be divisible by tp_size.
