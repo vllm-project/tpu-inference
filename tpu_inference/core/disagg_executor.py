bba65a38fc483dfafaf01cc4ce21b5b99891053e
Jacob Platin <jacobplatn@google.com>
2025-10-27 22:43:44 +0000

    Init commit
    Signed-off-by: Jacob Platin <jacobplatn@google.com>

--- tpu_inference/core/disagg_executor.py
+++ tpu_inference/core/disagg_executor.py
@@ -6,11 +6,11 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from vllm.logger import init_logger
 from vllm.multimodal import MULTIMODAL_REGISTRY
 from vllm.multimodal.cache import worker_receiver_cache_from_config
-from vllm.utils import run_method
 from vllm.utils.network_utils import (get_distributed_init_method, get_ip,
                                       get_open_port)
 from vllm.v1.executor.abstract import Executor
 from vllm.v1.outputs import AsyncModelRunnerOutput
+from vllm.v1.serial_utils import run_method
 from vllm.v1.worker.worker_base import WorkerWrapperBase
 
 logger = init_logger(__name__)
