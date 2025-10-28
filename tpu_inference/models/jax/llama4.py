5082a678ecb57c9a5fbe05a4174bcfdf121b071c
Sierra Qian <shengjieqian1225@gmail.com>
2025-10-28 23:38:42 +0000

    run pre commit

--- tpu_inference/models/jax/llama4.py
+++ tpu_inference/models/jax/llama4.py
@@ -442,7 +442,8 @@ class Llama4WeightLoader:
         cast_type = jnp.dtype(jnp.bfloat16)
         # loaded_weight is a jax.Array when framework="flax", otherwise it's bfloat16
         if not isinstance(loaded_weight, jax.Array):
-            loaded_weight = self._convert_torch_to_jax_with_view(loaded_weight, cast_type)
+            loaded_weight = self._convert_torch_to_jax_with_view(
+                loaded_weight, cast_type)
 
         split_weights = jnp.split(loaded_weight, 2, axis=-1)
         layer_num = self._get_layer_num(loaded_name)
@@ -531,7 +532,8 @@ class Llama4WeightLoader:
                         else:
                             cast_type = model_weight.array.qvalue.value.dtype
 
-                        loaded_weight = self._convert_torch_to_jax_with_view(loaded_weight, cast_type)
+                        loaded_weight = self._convert_torch_to_jax_with_view(
+                            loaded_weight, cast_type)
                         loaded_weight = transpose_params(
                             loaded_name, loaded_weight, self._transpose_map)
 
@@ -561,7 +563,8 @@ class Llama4WeightLoader:
                     logger.debug(
                         f"Converting PyTorch tensor {loaded_name} to JAX {cast_type}"
                     )
-                    loaded_weight = self._convert_torch_to_jax_with_view(loaded_weight, cast_type)
+                    loaded_weight = self._convert_torch_to_jax_with_view(
+                        loaded_weight, cast_type)
 
                 if not loaded_name.endswith(".bias"):
                     loaded_weight = reshape_params(loaded_name, loaded_weight,
