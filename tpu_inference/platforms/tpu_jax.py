f87550c0a7b3b58204330ad2774bc726e7dc5b35
Pooya Moradi <pooyam@google.com>
2025-10-31 06:57:54 +0000

    [Misc] Remove unused interfaces
    Signed-off-by: Pooya Moradi <pooyam@google.com>

--- tpu_inference/platforms/tpu_jax.py
+++ tpu_inference/platforms/tpu_jax.py
@@ -51,8 +51,7 @@ class TpuPlatform(Platform):
     additional_env_vars: list[str] = [
         "JAX_RANDOM_WEIGHTS", "PHASED_PROFILING_DIR",
         "TPU_CHIPS_PER_HOST_BOUNDS", "TPU_HOST_BOUNDS",
-        "TPU_MULTIHOST_BACKEND", "VLLM_MLA_DISABLE", "NEW_MODEL_DESIGN",
-        "TPU_BACKEND_TYPE"
+        "TPU_MULTIHOST_BACKEND", "VLLM_MLA_DISABLE", "TPU_BACKEND_TYPE"
     ]
 
     @classmethod
