1d47845a447ea044a1390333c091f267a5839b0e
Xing Liu <xingliu14@gmail.com>
2025-11-06 00:35:57 +0000

    Set VLLM_DISABLE_SHARED_EXPERTS_STREAM=1 by default for TPU inference
    Signed-off-by: Xing Liu <xingliu14@gmail.com>

--- tpu_inference/__init__.py
+++ tpu_inference/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+# Set default environment variables for TPU inference
+os.environ.setdefault('VLLM_DISABLE_SHARED_EXPERTS_STREAM', '1')
+
 from tpu_inference import tpu_info as ti
 from tpu_inference.logger import init_logger
 
