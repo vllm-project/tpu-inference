5f6d7579d09ec0149eea73bc4dc43684b6482df4
Xing Liu <xingliu14@gmail.com>
2025-11-07 07:08:54 +0000

    Fix isort formatting
    Signed-off-by: Xing Liu <xingliu14@gmail.com>

--- tpu_inference/__init__.py
+++ tpu_inference/__init__.py
@@ -4,7 +4,6 @@ import os
 # modules to ensure that the environment variables are set before any
 # other modules are imported.
 import tpu_inference.env_override  # noqa: F401
-
 from tpu_inference import tpu_info as ti
 from tpu_inference.logger import init_logger
 
