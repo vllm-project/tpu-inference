999f15c8747511b519837b7345d286a2baa3e401
Karan Goel <3261985+karan@users.noreply.github.com>
2025-12-02 10:30:29 -0800

    Increase threshold of perf test sensitivity to 6% (#1218)
    Signed-off-by: Karan Goel <karangoel@google.com>

--- tests/e2e/test_model_loader.py
+++ tests/e2e/test_model_loader.py
@@ -236,8 +236,8 @@ def test_flax_nnx_vs_vllm_performance():
     difference is within a reasonable threshold.
     """
     model_name = "Qwen/Qwen3-4B"
-    # This should be 2-3% but 5% reduces flakiness.
-    percentage_difference_threshold = 0.05
+    # This should be 2-3% but 6% reduces flakiness.
+    percentage_difference_threshold = 0.06
 
     throughput_vllm = _run_server_and_bench(model_name, "vllm", 8001)
     throughput_flax = _run_server_and_bench(model_name, "flax_nnx", 8002)
