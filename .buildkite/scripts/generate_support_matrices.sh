0956e2b9a4b500b68c574827b9dec0a661c143a7
Teresa Chen <boe20211@gmail.com>
2025-12-05 10:48:13 +0800

    test build
    Signed-off-by: Teresa Chen <boe20211@gmail.com>

--- .buildkite/scripts/generate_support_matrices.sh
+++ .buildkite/scripts/generate_support_matrices.sh
@@ -123,7 +123,7 @@ process_features() {
             is_quantization_matrix=true
             stages_to_use=("${FEATURE_STAGES_QUANTIZATION[@]}")
             header="Quantization dtype,Quantization methods,Recommended TPU Generations,CorrectnessTest,PerformanceTest"
-        elif [ "$category" == "kernel support matrix (microbenchmarks)" ]; then
+        elif [ "$category" == "kernel_support_matrix_(microbenchmarks)" ]; then
             is_kernel_microbenchmarks=true
             stages_to_use=("${FEATURE_STAGES_MICROBENCHMARKS[@]}")
         fi
