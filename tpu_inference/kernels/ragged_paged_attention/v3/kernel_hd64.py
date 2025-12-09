45b6df15417e7fcfbf9b2f9675d86d494f85eafd
Kyuyeun Kim <kyuyeunk@google.com>
2025-12-09 23:41:05 +0000

    2k/6k tuned
    Signed-off-by: Kyuyeun Kim <kyuyeunk@google.com>

--- tpu_inference/kernels/ragged_paged_attention/v3/kernel_hd64.py
+++ tpu_inference/kernels/ragged_paged_attention/v3/kernel_hd64.py
@@ -1443,8 +1443,8 @@ def ragged_paged_attention_hd64(
             pages_per_seq,
         )
 
-    bq_sz = 16
-    bkv_p = 24
+    bq_sz = 8
+    bkv_p = 16
     if sliding_window is not None:
         bkv_p = 4
 
