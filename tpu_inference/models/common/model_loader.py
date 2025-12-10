812132ac5c772b1d3924c47eb280c7769c14c61b
Aman Gupta <amangu@google.com>
2025-12-10 13:30:50 -0800

    Fixing changes suggested by yapf in the pre-commit.
    Signed-off-by: Aman Gupta <amangu@google.com>

--- tpu_inference/models/common/model_loader.py
+++ tpu_inference/models/common/model_loader.py
@@ -24,6 +24,7 @@ logger = init_logger(__name__)
 
 _MODEL_REGISTRY = {}
 
+
 class UnsupportedArchitectureError(ValueError):
     """Raised when a model architecture is not supported in the registry."""
     pass
