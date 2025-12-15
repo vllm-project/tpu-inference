e992628726d2a9df4c59d320c850b30698f904b7
Sneha Aradhey <snehaaradhey@google.com>
2025-12-15 21:51:58 +0000

    add testcases to validate both prompt size less and greater than CPU RAM
    Signed-off-by: Sneha Aradhey <snehaaradhey@google.com>

--- tests/offload/tpu_offload_accuracy_test.py
+++ tests/offload/tpu_offload_accuracy_test.py
@@ -51,19 +51,22 @@ def _test_kv_cache_cpu_offloading_accuracy(
     swap_op_type: str,
     skip_precompile: str,
     decode_save: str,
+    cpu_chunks: str,
+    prompt_file: str,
 ):
     with monkeypatch.context():
         os.environ['SKIP_JAX_PRECOMPILE'] = '1'
         os.environ['TPU_OFFLOAD_SWAP_OP_TYPE'] = swap_op_type
         os.environ['TPU_OFFLOAD_SKIP_JAX_PRECOMPILE'] = skip_precompile
         os.environ['TPU_OFFLOAD_DECODE_SAVE'] = decode_save
+        os.environ['TPU_OFFLOAD_NUM_CPU_CHUNKS'] = cpu_chunks
         llm = LLM(model="meta-llama/Llama-3.2-3B",
-                  max_model_len=1024,
+                  max_model_len=3072,
                   task="generate",
                   kv_transfer_config=kv_transfer_config)
 
+        prompt = read_prompt_from_file(prompt_file)
         # 1st generate
-        prompt = "Every Bill which shall have passed the House of Representatives and the Senate, shall, before it become a Law, be presented to the President of the United States; If he approve he shall sign it, but if not he shall return it, with his Objections to that House in which it shall have originated, who shall enter the Objections at large on their Journal, and proceed to reconsider it. If after such Reconsideration two thirds of that House shall agree to pass the Bill, it shall be sent, together with the Objections, to the other House, by which it shall likewise be reconsidered, and if approved by two thirds of that House, it shall become a Law. But in all such Cases the Votes of both Houses shall be determined by yeas and Nays, and the Names of the Persons voting for and against the Bill shall be entered on the Journal of each House respectively. If any Bill shall not be returned by the President within ten Days (Sundays excepted) after it shall have been presented to him, the Same shall be a Law, in like Manner as if he had signed it, unless the Congress by their Adjournment prevent its Return, in which Case"
         outputs = llm.generate([prompt], sampling_config)
         out_texts1, out_tokens1 = parse_outputs(outputs)
         time.sleep(1)
@@ -78,7 +81,7 @@ def _test_kv_cache_cpu_offloading_accuracy(
         time.sleep(1)
 
         # TODO(jcgu): check some internal states to verify save and load operations.
-        # output1 and output2 should be idential
+        # output1 and output2 should be identical
         assert len(out_texts1) == len(out_texts2)
         assert len(out_tokens1) == len(out_tokens2)
         for text1, text2 in zip(out_texts1, out_texts2):
@@ -91,7 +94,7 @@ def _test_kv_cache_cpu_offloading_accuracy(
         time.sleep(20)
 
 
-def test_kv_cache_cpu_offloading_accuracy(
+def test_kv_cache_cpu_offloading_accuracy_smaller_then_cpu_ram(
     monkeypatch: pytest.MonkeyPatch,
     sampling_config: SamplingParams,
     kv_transfer_config: KVTransferConfig,
@@ -108,4 +111,34 @@ def test_kv_cache_cpu_offloading_accuracy(
             swap_op_type,
             _skip_precompile,
             decode_save,
+            "24",
+            "small_prompt.txt",
         )
+
+def test_kv_cache_cpu_offloading_accuracy_larger_than_cpu_ram(
+    monkeypatch: pytest.MonkeyPatch,
+    sampling_config: SamplingParams,
+    kv_transfer_config: KVTransferConfig,
+):
+    swap_op_types = ["pallas", "jax"]
+    decode_saves = ["0", "1"]
+    skip_precompile = ["0", "1"]
+    for swap_op_type, decode_save, _skip_precompile in itertools.product(
+            swap_op_types, decode_saves, skip_precompile):
+        _test_kv_cache_cpu_offloading_accuracy(
+            monkeypatch,
+            sampling_config,
+            kv_transfer_config,
+            swap_op_type,
+            _skip_precompile,
+            decode_save,
+            "8",
+            "large_prompt.txt",
+        )
+        
+def read_prompt_from_file(file_name):
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "prompt_files", file_name)
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
