30fc1fb4b2acf8ef78ea1bcb8f0e2d23fa9f4467
kunjan <kunjan@ucla.edu>
2025-12-17 02:34:34 +0000

    fix tests, simplify condition for replication of rhs scale in rowwise parallel
    Signed-off-by: kunjan <kunjan@ucla.edu>

--- tests/layers/vllm/test_compressed_tensors_moe.py
+++ tests/layers/vllm/test_compressed_tensors_moe.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
 
-import jax
 import jax.numpy as jnp
 import pytest
 import torch
@@ -89,9 +88,8 @@ def _ref_math_in_bf16(w1, w2, w3, x, router_logits, top_k):
 @pytest.mark.parametrize("num_experts", [8])
 @pytest.mark.parametrize("topk", [2])
 @pytest.mark.parametrize("use_ep", [True, False])
-def test_fused_moe_method(use_ep):
-    mesh = test_utils.get_spmd_mesh(jax.local_device_count())
-
+def test_fused_moe_method(mesh, num_tokens, intermediate_size, hidden_size,
+                          num_experts, topk, use_ep):
     engine_args = EngineArgs(
         model=MODEL,
         max_model_len=64,
@@ -105,14 +103,9 @@ def test_fused_moe_method(use_ep):
     vllm_config.model_config.dtype = torch.bfloat16
     quant_config = get_tpu_quantization_config(vllm_config, mesh)
 
-    num_experts = 8
-    top_k = 2
-    hidden_size = 128
-    intermediate_size = hidden_size * 2
-
     with set_current_vllm_config(vllm_config):
         layer = FusedMoE(num_experts=num_experts,
-                         top_k=top_k,
+                         top_k=topk,
                          hidden_size=hidden_size,
                          intermediate_size=intermediate_size)
     quant_config = VllmCompressedTensorsConfig(
@@ -150,10 +143,10 @@ def test_fused_moe_method(use_ep):
         sparsity_ignore_list=[],
     )
     moe = FusedMoEConfig(
-        num_experts=8,
-        experts_per_token=2,
+        num_experts=num_experts,
+        experts_per_token=topk,
         hidden_dim=hidden_size,
-        num_local_experts=8,
+        num_local_experts=num_experts,
         moe_parallel_config=FusedMoEParallelConfig(
             tp_size=1,
             dp_size=1,
@@ -174,7 +167,7 @@ def test_fused_moe_method(use_ep):
                           params_dtype=torch.float8_e4m3fn)
     method.process_weights_after_loading(layer)
 
-    seqlen = 10
+    seqlen = num_tokens
     with torchax.default_env():
         x = torch.ones((seqlen, hidden_size), dtype=torch.bfloat16).to('jax')
         router_logits = torch.randn((seqlen, num_experts),
@@ -182,13 +175,13 @@ def test_fused_moe_method(use_ep):
         result = method.apply(layer,
                               x,
                               router_logits,
-                              top_k=2,
+                              top_k=topk,
                               renormalize=True)
 
         result_reference = _ref_math_in_bf16(
             layer.w13_weight.to(torch.bfloat16) * layer.w13_weight_scale,
             layer.w2_weight.to(torch.bfloat16) * layer.w2_weight_scale,
             layer.w3_weight.to(torch.bfloat16) * layer.w3_weight_scale, x,
-            router_logits, top_k)
+            router_logits, topk)
 
         assert jnp.allclose(result.jax(), result_reference.jax())
