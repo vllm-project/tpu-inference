6f4eee1680c171fa70a690617520007fd8331280
Your Name <you@example.com>
2025-12-30 19:30:30 +0000

    Add custom change

--- tpu_inference/platforms/tpu_platform.py
+++ tpu_inference/platforms/tpu_platform.py
@@ -46,8 +46,9 @@ class TpuPlatform(Platform):
 
     additional_env_vars: list[str] = [
         "PHASED_PROFILING_DIR", "TPU_CHIPS_PER_HOST_BOUNDS", "TPU_HOST_BOUNDS",
-        "TPU_MULTIHOST_BACKEND", "VLLM_MLA_DISABLE", "TPU_BACKEND_TYPE",
-        "NEW_MODEL_DESIGN"
+        "TPU_MULTIHOST_BACKEND", "VLLM_MLA_DISABLE", "TPU_BACKEND_TYPE",        
+        "NEW_MODEL_DESIGN", "VLLM_DISABLE_SHARED_EXPERTS_STREAM",
+        "MODEL_IMPL_TYPE"
     ]
 
     @classmethod
