9b31896a92a7d84c8d15993564b2a8933cd181e3
catswe <212922539+catswe@users.noreply.github.com>
2026-01-01 21:38:34 +0000

    Add quant override test
    Signed-off-by: catswe <212922539+catswe@users.noreply.github.com>

--- tests/layers/vllm/test_fp8.py
+++ tests/layers/vllm/test_fp8.py
@@ -12,6 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
+import tempfile
 
-pytest.skip("FP8 implementation not complete yet", allow_module_level=True)
+import jax
+import pytest
+import torch
+from jax.sharding import PartitionSpec
+from vllm.config import ParallelConfig, set_current_vllm_config
+from vllm.distributed.parallel_state import (ensure_model_parallel_initialized,
+                                             init_distributed_environment)
+from vllm.engine.arg_utils import EngineArgs
+
+from tpu_inference.layers.vllm.quantization import get_tpu_quantization_config
+from tpu_inference.layers.vllm.quantization.fp8 import (VllmFp8Config,
+                                                          VllmFp8MoEMethod)
+
+from . import utils as test_utils
+
+P = PartitionSpec
+MODELS = ["Qwen/Qwen3-0.6B-FP8"]
+
+
+@pytest.fixture(autouse=True)
+def setup_environment():
+    # This is a fake config used for init dist env.
+    # RowParallelLinear needs dist env to be initialized.
+    engine_args = EngineArgs(
+        model=MODELS[0],
+        max_model_len=64,
+        max_num_batched_tokens=64,
+        max_num_seqs=4,
+    )
+
+    vllm_config = engine_args.create_engine_config()
+
+    with set_current_vllm_config(vllm_config):
+        temp_file = tempfile.mkstemp()[1]
+        init_distributed_environment(
+            1,
+            0,
+            local_rank=0,
+            distributed_init_method=f"file://{temp_file}",
+            backend="gloo")
+        ensure_model_parallel_initialized(1, 1)
+
+
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("mesh", [
+    test_utils.get_spmd_mesh(1),
+    test_utils.get_spmd_mesh(jax.local_device_count())
+])
+def test_quant_override(model, mesh):
+
+    engine_args = EngineArgs(
+        model=model,
+        max_model_len=64,
+        max_num_batched_tokens=64,
+        max_num_seqs=4,
+    )
+    vllm_config = engine_args.create_engine_config()
+    vllm_config.model_config.dtype = torch.bfloat16
+
+    quant_config = get_tpu_quantization_config(vllm_config, mesh)
+    assert isinstance(quant_config, VllmFp8Config)
+    assert quant_config.vllm_config == vllm_config
+    assert quant_config.mesh == mesh
+
