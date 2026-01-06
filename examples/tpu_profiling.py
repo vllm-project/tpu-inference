160ee1cb429f11449ca6dcdc3416ed4a7856dcf0
oliver <44170519+oliverdutton@users.noreply.github.com>
2026-01-06 18:12:50 +0700

    Fix profiling script
    Signed-off-by: oliver <44170519+oliverdutton@users.noreply.github.com>

--- examples/tpu_profiling.py
+++ examples/tpu_profiling.py
@@ -47,10 +47,9 @@ def main(args: argparse.Namespace):
         max_tokens=args.output_len,
     )
     print(sampling_params)
-    dummy_prompt_token_ids = np.random.randint(10000,
-                                               size=(args.batch_size,
-                                               size=(batch_size,
-                                                     args.input_len))
+    dummy_prompt_token_ids = np.random.randint(
+    10000,
+    size=(args.batch_size, args.input_len))
     dummy_prompts: list[PromptType] = [{
         "prompt_token_ids": batch
     } for batch in dummy_prompt_token_ids.tolist()]
