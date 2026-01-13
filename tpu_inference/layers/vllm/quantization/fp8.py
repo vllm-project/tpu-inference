84b81af433e7cc53c9cef3c583a0f8eb41557abb
catswe <212922539+catswe@users.noreply.github.com>
2026-01-12 21:20:27 -0600

    Clean up
    Signed-off-by: catswe <212922539+catswe@users.noreply.github.com>

--- tpu_inference/layers/vllm/quantization/fp8.py
+++ tpu_inference/layers/vllm/quantization/fp8.py
@@ -175,7 +175,6 @@ class VllmFp8LinearMethod(Fp8LinearMethod):
                 weight_slice = weight[start:end]
                 weight_scale_slice = weight_scale[start // block_size:math.
                                                   ceil(end / block_size)]
-
                 dequantized_weight = _pad_and_dequantize(
                     weight_slice,
                     weight_scale_slice,
