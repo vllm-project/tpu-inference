a762c920ac49b4a072de143e38d9f8c9c742deed
Linkun Chen <github@lkchen.net>
2026-01-21 22:15:24 +0000

    [Quantization] Move some logic from layers/vllm/quantization to layser/common/quantization
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- tpu_inference/layers/vllm/quantization/unquantized.py
+++ tpu_inference/layers/vllm/quantization/unquantized.py
@@ -16,12 +16,13 @@ from typing import Any, Optional
 
 import jax
 import torch
-import vllm.model_executor.layers.linear as vllm_linear
+
 from jax.sharding import Mesh, NamedSharding, PartitionSpec
 from torch.nn.parameter import Parameter
 from torchax.interop import jax_view, torch_view
 from torchax.ops.mappings import t2j
 from vllm.attention.layer import Attention
+from vllm.model_executor.layers import linear as vllm_linear
 from vllm.model_executor.layers.fused_moe import (FusedMoE, FusedMoEConfig,
                                                   UnquantizedFusedMoEMethod)
 from vllm.model_executor.layers.quantization import \
