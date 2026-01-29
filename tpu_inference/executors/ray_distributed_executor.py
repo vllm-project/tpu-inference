c3ada0c07e3a206a6e828ac3f7f50e3d00c2955f
mrjunwan-lang <mrjunwan@google.com>
2026-01-29 22:43:26 +0000

    fix the typo

--- tpu_inference/executors/ray_distributed_executor.py
+++ tpu_inference/executors/ray_distributed_executor.py
@@ -355,7 +355,9 @@ class RayDistributedExecutor(RayDistributedExecutorV1):
             # worker-specific config copy and restore the original GCS URI from `model_weights`.
             # This allows each worker to independently invoke `maybe_pull_model_tokenizer_for_runai`
             # and stream the model from GCS.
-            if node_id != driver_node_id and self.vllm_config.model_config.model_weights and self.model_config.maybe_pull_model_tokenizer_for_runai:
+            if node_id != driver_node_id and hasattr(
+                    self.vllm_config, "model_config"
+            ) and self.vllm_config.model_config.model_weights:
                 worker_vllm_config = copy.deepcopy(self.vllm_config)
                 worker_vllm_config.model_config.model = worker_vllm_config.model_config.model_weights
                 # Unset model_weights so maybe_pull_model_tokenizer_for_runai will pull the model.
