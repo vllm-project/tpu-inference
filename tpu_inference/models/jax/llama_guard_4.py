2839929c3b18045566ee1473cd54522caf0e4991
Chenyaaang <chenyangli@google.com>
2026-02-03 05:35:50 +0000

    disable yapf and isort in llama guard 4 file
    Signed-off-by: Chenyaaang <chenyangli@google.com>

--- tpu_inference/models/jax/llama_guard_4.py
+++ tpu_inference/models/jax/llama_guard_4.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# yapf: disable
+# isort: skip_file
+
 import re
 from itertools import islice
 from typing import Any, List, Optional, Tuple
