05102b3b166491d368df8d3c7a5c4f7a2d1eef75
Linkun Chen <github@lkchen.net>
2026-02-04 05:44:08 +0000

    test unquant.
    Signed-off-by: Linkun Chen <github@lkchen.net>

---
+++ tests/layers/jax/quantization/test_unquantized.py
@@ -0,0 +1,54 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jax
+import numpy as np
+import pytest
+from flax import nnx
+
+from tpu_inference.layers.jax.linear import JaxLinear
+from tpu_inference.layers.jax.quantization import QuantizeMethodBase
+from tpu_inference.layers.jax.quantization.unquantized import UnquantizedConfig
+
+
+@pytest.fixture
+def rngs():
+    return nnx.Rngs(42)
+
+
+class TestUnquantizedJaxLinear:
+
+    @pytest.mark.parametrize("in_features,out_features", [(4, 6), (8, 16)])
+    @pytest.mark.parametrize("use_bias", [True, False])
+    @pytest.mark.parametrize("batch_size", [1, 4])
+    def test_linear_forward_correctness(self, in_features, out_features,
+                                        use_bias, batch_size, rngs):
+        # Create input data with rngs
+        x = jax.random.uniform(rngs.params(), (batch_size, in_features))
+
+        jax_linear = JaxLinear(in_features,
+                               out_features,
+                               rngs,
+                               use_bias=use_bias)
+        y_from_layer = jax_linear(x)
+
+        method = UnquantizedConfig().get_quant_method(jax_linear, prefix='')
+        assert isinstance(method, QuantizeMethodBase)
+        y_from_method = method.apply_jax(jax_linear, x)
+
+        # compare outputs
+        np.testing.assert_allclose(y_from_layer,
+                                   y_from_method,
+                                   rtol=1e-5,
+                                   atol=1e-5)
