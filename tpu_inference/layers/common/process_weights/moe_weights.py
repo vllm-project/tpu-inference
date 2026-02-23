0686863ce98b6e09e6a5e8d65b1e56855e14e39f
Kyuyeun Kim <kyuyeunk@google.com>
2026-02-23 08:52:28 +0000

    fp4 moe weights
    Signed-off-by: Kyuyeun Kim <kyuyeunk@google.com>

--- tpu_inference/layers/common/process_weights/moe_weights.py
+++ tpu_inference/layers/common/process_weights/moe_weights.py
@@ -432,8 +432,10 @@ def process_fp8_moe_weights(
             w2_weight_scale=None,
             w2_bias=None,
         ),
-        jnp.float8_e4m3fn,
-        None,
+        # jnp.float8_e4m3fn,
+        # None,
+        jnp.float4_e2m1fn,
+        512,
     )
     return process_moe_weights(
         weights,
