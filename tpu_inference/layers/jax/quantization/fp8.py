59e15b076a8f9ed1a7b1d508fcfdf209b4efb48f
Linkun Chen <github@lkchen.net>
2026-03-01 07:24:45 +0000

    set _loaded
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- tpu_inference/layers/jax/quantization/fp8.py
+++ tpu_inference/layers/jax/quantization/fp8.py
@@ -43,8 +43,8 @@ from tpu_inference.layers.jax.quantization.unquantized import (
     UnquantizedFusedMoEMethod, UnquantizedLinearMethod)
 from tpu_inference.logger import init_logger
 from tpu_inference.models.jax.utils.weight_utils import (
-    cpu_mesh, cpu_mesh_context, jax_array_from_reshaped_torch,
-    load_nnx_param_from_reshaped_torch, shard_put)
+    jax_array_from_reshaped_torch, load_nnx_param_from_reshaped_torch,
+    shard_put)
 
 logger = init_logger(__name__)
 
