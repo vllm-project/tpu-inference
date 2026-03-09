c131fbdc8f173b17662cc8d74ab8b2a198cadb21
Kunjan Patel <kunjan@ucla.edu>
2026-03-09 17:08:04 +0000

    Activation fusion in gmm
    Signed-off-by: Kunjan Patel <kunjan@ucla.edu>

--- tpu_inference/layers/common/fused_moe_gmm.py
+++ tpu_inference/layers/common/fused_moe_gmm.py
@@ -61,8 +61,17 @@ def _swigluoai(x1: jax.Array,
     return gated_activation * (x2 + 1)
 
 
-def gmm_wrapper(lhs, rhs, rhs_scale, rhs_bias, group_sizes, group_offset,
-                last_gmm):
+def gmm_wrapper(lhs,
+                rhs,
+                rhs_scale,
+                rhs_bias,
+                group_sizes,
+                group_offset,
+                last_gmm,
+                fuse_act=None):
+    # if is_supported_by_gmm_v2(rhs_scale) and (not last_gmm
+    #                                           or lhs.shape[0] < 1024):
+   
     gmm_res = gmm_v2(
         lhs=lhs,
         rhs=rhs,
@@ -73,6 +82,7 @@ def gmm_wrapper(lhs, rhs, rhs_scale, rhs_bias, group_sizes, group_offset,
         # If it's last gmm, we need to zero out unvisited rows because it would
         # cause numeric error during final reduce if the rows are unitialized.
         zero_initialize=last_gmm,
+        fuse_act=fuse_act,
     )
     return gmm_res
 
@@ -103,10 +113,16 @@ def moe_gmm_local(
 
     # GMM1 computes x @ (W_up | W_gate) tegether and then split out to apply activation
     # to the gate result
-    gmm1_res_gate_up = gmm_wrapper(x, w1, w1_scale, w1_bias, group_sizes,
-                                   group_offset, False)
-    gmm1_res_gate, gmm1_res_up = jnp.split(gmm1_res_gate_up, 2, -1)
-    gmm1_res = apply_act_fn(activation, gmm1_res_gate, gmm1_res_up)
+    gmm1_res = gmm_wrapper(x,
+                           w1,
+                           w1_scale,
+                           w1_bias,
+                           group_sizes,
+                           group_offset,
+                           False,
+                           fuse_act=activation)
+    # gmm1_res_gate, gmm1_res_up = jnp.split(gmm1_res_gate_up, 2, -1)
+    # gmm1_res = apply_act_fn(activation, gmm1_res_gate, gmm1_res_up)
 
     # When the parallelism is TP since w2_bias is not sharded, we should only apply bias
     # once, not applying to every shard. So we set w2_bias to 0 to all shards other than
@@ -114,7 +130,7 @@ def moe_gmm_local(
     if parallelism == "tp" and w2_bias is not None:
         shard_id = jax.lax.axis_index(ShardingAxisName.MLP_TENSOR).sum()
         w2_bias = jnp.where(shard_id == 0, w2_bias, 0)
-
+    gmm1_res = gmm1_res[:, :w2.shape[1]]  # trim to hidden size if padded
     gmm2_res = gmm_wrapper(gmm1_res, w2, w2_scale, w2_bias, group_sizes,
                            group_offset, True)
 
