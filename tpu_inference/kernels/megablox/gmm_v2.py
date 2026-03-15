759d18f0451117e3cea2845300c07bba30f6866c
catswe <212922539+catswe@users.noreply.github.com>
2026-03-15 15:42:03 -0500

    Fix incorrect RHS scale application in GMM kernel
    Signed-off-by: catswe <212922539+catswe@users.noreply.github.com>

--- tpu_inference/kernels/megablox/gmm_v2.py
+++ tpu_inference/kernels/megablox/gmm_v2.py
@@ -923,7 +923,8 @@ def make_gmm_configs(
         # scale value. Since this operation is very memory intensive, we use a
         # block size that is small enough to minimize memory overhead but large
         # enough to minimize compute overhead of quantization.
-        quant_block_size=512,
+        quant_block_size=rhs_cfgs.quant_block_size
+        if rhs_cfgs.has_scale else 512,
         dtype=lhs.dtype,
     )
 
