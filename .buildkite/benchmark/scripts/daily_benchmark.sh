2cfd1c385b52463dfe75e45aea2528a53a7a045b
dennis yeh <dennis.yeh@cienet.com>
2026-03-19 17:13:33 +0800

    convert dev csv
    Signed-off-by: dennis yeh <dennis.yeh@cienet.com>

--- .buildkite/benchmark/scripts/daily_benchmark.sh
+++ .buildkite/benchmark/scripts/daily_benchmark.sh
@@ -34,26 +34,25 @@ upload_benchmark_yml() {
 }
 
 # Upload Only one test case for Dev
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/benchmark_dev_test_v7x.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/benchmark_dev_test_v7x.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
+upload_benchmark_yml .buildkite/benchmark/cases/benchmark_dev_test_v7x.yml
 
-# 1. Ironwood qwen & Llama
-upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.yml
+# # 1. Ironwood qwen & Llama
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.yml
 
-# 2. Qwen3-32B random benchmarks
-upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.yml
+# # 2. Qwen3-32B random benchmarks
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.yml
 
-# 3. Ironwood Deepseek DP Attention
-upload_benchmark_yml .buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.yml
+# # 3. Ironwood Deepseek DP Attention
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.yml
 
-# 4. Ironwood Deepseek
-upload_benchmark_yml .buildkite/benchmark/cases/daily_deepseek_tpu7x_8.yml
+# # 4. Ironwood Deepseek
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_deepseek_tpu7x_8.yml
 
-# 5. Ironwood Deepseek Accuracy
-upload_benchmark_yml .buildkite/benchmark/cases/accuracy_jax_v7x.yml
+# # 5. Ironwood Deepseek Accuracy
+# upload_benchmark_yml .buildkite/benchmark/cases/accuracy_jax_v7x.yml
 
-# 6. GPT OSS
-upload_benchmark_yml .buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.yml
+# # 6. GPT OSS
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.yml
 
-# 7. Qwen 3-480B
-upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.yml
+# # 7. Qwen 3-480B
+# upload_benchmark_yml .buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.yml
