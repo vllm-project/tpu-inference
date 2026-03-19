9aea7475a16a338fe006e2374c81312def8cd27e
StingLin <sting.lin@cienet.com>
2026-03-19 12:11:24 +0800

    Adjust for only test one case
    Signed-off-by: StingLin <sting.lin@cienet.com>

--- .buildkite/benchmark/scripts/daily_benchmark.sh
+++ .buildkite/benchmark/scripts/daily_benchmark.sh
@@ -20,32 +20,36 @@ JOB_PRIORITY=$(buildkite-agent meta-data get "JOB_PRIORITY")
 
 echo "Benchmark pipeline prioirty: $JOB_PRIORITY"
 
+# Upload Only one test case for Dev
+echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/benchmark_dev_test_v7x.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
+./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/benchmark_dev_test_v7x.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
+
 # # Ironwood qwen & Llama
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen_llama_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
 
-# Qwen3-32B random benchmarks (Using benchmark_serving)
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=64;\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=64;"
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=320;\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=320;"
+# # Qwen3-32B random benchmarks (Using benchmark_serving)
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=64;\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=64;"
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=320;\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_32B_random_tpu7x_2.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_BENCHMARK_SERVING=1;MAX_CONCURRENCY=320;"
 
-# Ironwood Deepseek DP Attention
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"VLLM_MLA_DISABLE=0;NEW_MODEL_DESIGN=1;MOE_REQUANTIZE_BLOCK_SIZE=512;MOE_REQUANTIZE_WEIGHT_DTYPE=fp4;TPU_BACKEND_TYPE=jax;MODEL_IMPL_TYPE=vllm;\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=0;NEW_MODEL_DESIGN=1;MOE_REQUANTIZE_BLOCK_SIZE=512;MOE_REQUANTIZE_WEIGHT_DTYPE=fp4;TPU_BACKEND_TYPE=jax;MODEL_IMPL_TYPE=vllm;"
+# # Ironwood Deepseek DP Attention
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"VLLM_MLA_DISABLE=0;NEW_MODEL_DESIGN=1;MOE_REQUANTIZE_BLOCK_SIZE=512;MOE_REQUANTIZE_WEIGHT_DTYPE=fp4;TPU_BACKEND_TYPE=jax;MODEL_IMPL_TYPE=vllm;\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_dp_attention_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=0;NEW_MODEL_DESIGN=1;MOE_REQUANTIZE_BLOCK_SIZE=512;MOE_REQUANTIZE_WEIGHT_DTYPE=fp4;TPU_BACKEND_TYPE=jax;MODEL_IMPL_TYPE=vllm;"
 
-# Ironwood Deepseek
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"VLLM_MLA_DISABLE=1;TPU_BACKEND_TYPE=vllm\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=1;TPU_BACKEND_TYPE=vllm"
+# # Ironwood Deepseek
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"VLLM_MLA_DISABLE=1;TPU_BACKEND_TYPE=vllm\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_deepseek_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=1;TPU_BACKEND_TYPE=vllm"
 
 # # Ironwood Deepseek Accuracy
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/accuracy_jax_v7x.csv $CODE_HASH $TAG JAX_ACCURACY $JOB_PRIORITY \"VLLM_MLA_DISABLE=1;NEW_MODEL_DESIGN=True;TPU_BACKEND_TYPE=jax;\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/accuracy_jax_v7x.csv "$CODE_HASH" "$TAG" JAX_ACCURACY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=1;NEW_MODEL_DESIGN=True;TPU_BACKEND_TYPE=jax;"
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/accuracy_jax_v7x.csv $CODE_HASH $TAG JAX_ACCURACY $JOB_PRIORITY \"VLLM_MLA_DISABLE=1;NEW_MODEL_DESIGN=True;TPU_BACKEND_TYPE=jax;\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/accuracy_jax_v7x.csv "$CODE_HASH" "$TAG" JAX_ACCURACY "$JOB_PRIORITY" "VLLM_MLA_DISABLE=1;NEW_MODEL_DESIGN=True;TPU_BACKEND_TYPE=jax;"
 
-# GPT OSS
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_MOE_EP_KERNEL=0;MODEL_IMPL_TYPE=vllm\""
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_MOE_EP_KERNEL=0;MODEL_IMPL_TYPE=vllm"
+# # GPT OSS
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY \"USE_MOE_EP_KERNEL=0;MODEL_IMPL_TYPE=vllm\""
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_gpt_oss_120b_tpu7x.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY" "USE_MOE_EP_KERNEL=0;MODEL_IMPL_TYPE=vllm"
 
-# Qwen 3-480B
-echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
-./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
+# # Qwen 3-480B
+# echo "./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.csv $CODE_HASH $TAG DAILY $JOB_PRIORITY"
+# ./.buildkite/benchmark/scripts/schedule_run.sh ./.buildkite/benchmark/cases/daily_qwen3_480B_FP8_tpu7x_8.csv "$CODE_HASH" "$TAG" DAILY "$JOB_PRIORITY"
