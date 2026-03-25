55472e22adde39715bdfa81b6102aacdc366e565
Ernie Chang <changernie@google.com>
2026-03-25 10:35:40 +0800

    move test dependencies to requirement_test and test nightly on buildkite
    Signed-off-by: Ernie Chang <changernie@google.com>

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -115,7 +115,7 @@ upload_with_priority() {
 upload_pipeline() {
     if [ "${MODEL_IMPL_TYPE:-auto}" == "auto" ]; then
       # Upload JAX pipeline for v6 (default)
-      upload_with_priority .buildkite/pipeline_jax.yml
+      # upload_with_priority .buildkite/pipeline_jax.yml
 
       # Upload JAX pipeline for v7
       export TESTS_GROUP_LABEL="[jax] TPU7x Tests Group"
@@ -123,15 +123,15 @@ upload_pipeline() {
       export TPU_QUEUE_SINGLE="tpu_v7x_2_queue"
       export TPU_QUEUE_MULTI="tpu_v7x_8_queue"
       export COV_FAIL_UNDER="67"
-      upload_with_priority .buildkite/pipeline_jax.yml
+      # upload_with_priority .buildkite/pipeline_jax.yml
       unset TPU_VERSION TPU_QUEUE_SINGLE TPU_QUEUE_MULTI COV_FAIL_UNDER
 
       # buildkite-agent pipeline upload .buildkite/pipeline_torch.yml
-      upload_with_priority .buildkite/nightly_releases.yml
+      # upload_with_priority .buildkite/nightly_releases.yml
     fi
 
     upload_with_priority .buildkite/nightly_verify.yml
-    upload_with_priority .buildkite/pipeline_pypi.yml
+    # upload_with_priority .buildkite/pipeline_pypi.yml
 }
 
 echo "--- Starting Buildkite Bootstrap"
