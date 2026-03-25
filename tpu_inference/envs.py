d7babc47471ab5fff8cf80aa53419bb3fdc716c0
Yuyan Peng <yuyanpeng@google.com>
2026-03-25 07:49:35 +0000

    feat(torchax, mm): add env for jit some submodule in mm model for better performance
    Since the embed_multimodal include the preprocessing,
    which cannot and should not be jitted directly,
    add envs to give a chance to jit
    the module in the mm.
    
    - JITTED_MM_MODULE_KEYS for the module to jit
    - REGISTER_MM_MODULE_CUSTOM_PYTREE_CLASSES for the custom pytree class.
    
    Signed-off-by: Yuyan Peng <yuyanpeng@google.com>

--- tpu_inference/envs.py
+++ tpu_inference/envs.py
@@ -38,6 +38,8 @@ if TYPE_CHECKING:
     FORCE_MOE_RANDOM_ROUTING: bool = False
     SC_KERNEL_THRESHOLD: int = 16777216
     SC_KERNEL_COL_CHUNK_SIZE: int = 1024
+    JITTED_MM_MODULE_KEYS: list[str] = []
+    REGISTER_MM_MODULE_CUSTOM_PYTREE_CLASSES: list[str] = []
 
 
 def env_with_choices(
@@ -113,6 +115,25 @@ def env_bool(env_name: str, default: bool = False) -> Callable[[], bool]:
                 f"Valid options: '0', '1', 'true', 'false', 'True', 'False'.")
 
     return _get_bool_env
+
+
+def env_str_list(env_name: str) -> Callable[[], list[str]]:
+    """
+    Accepts a comma-separated string and returns a list of strings.
+
+    Args:
+        env_name: Name of the environment variable
+        default: Default list of strings if not set
+    """
+
+    def _get_str_list_env() -> list[str]:
+        value = os.getenv(env_name)
+        if value is None or value == "":
+            return []
+
+        return [v.strip() for v in value.split(",")]
+
+    return _get_str_list_env
 
 
 environment_variables: dict[str, Callable[[], Any]] = {
@@ -214,6 +235,10 @@ environment_variables: dict[str, Callable[[], Any]] = {
     lambda: int(os.getenv("SC_KERNEL_THRESHOLD") or "16777216"),
     "SC_KERNEL_COL_CHUNK_SIZE":
     lambda: int(os.getenv("SC_KERNEL_COL_CHUNK_SIZE") or "3072"),
+    "JITTED_MM_MODULE_KEYS":
+    env_str_list("JITTED_MM_MODULE_KEYS"),
+    "REGISTER_MM_MODULE_CUSTOM_PYTREE_CLASSES":
+    env_str_list("REGISTER_MM_MODULE_CUSTOM_PYTREE_CLASSES"),
 }
 
 
