917ddbbb2e6159a03b8b44d9500241f5f7b14e83
gpolovets1 <gpolovets@gmail.com>
2026-04-03 11:27:27 -0700

    DeepSeek RoPE TPU friendly Custom Op (#2132)
    Signed-off-by: George Polovets <gpolovets@gmail.com>
    Signed-off-by: clee1994 <clemensschaefer1994@googlemail.com>

--- tpu_inference/layers/vllm/custom_ops/__init__.py
+++ tpu_inference/layers/vllm/custom_ops/__init__.py
@@ -16,6 +16,7 @@ from tpu_inference.layers.vllm.custom_ops import embedding as embedding
 from tpu_inference.layers.vllm.custom_ops import fused_moe as fused_moe
 from tpu_inference.layers.vllm.custom_ops import linear as linear
 from tpu_inference.layers.vllm.custom_ops import mla_attention as mla_attention
+from tpu_inference.layers.vllm.custom_ops import rope as rope
 
 # Register custom op to vLLM so that vLLM model implementation will instantiante
 # classes with definitions in tpu-inference.
