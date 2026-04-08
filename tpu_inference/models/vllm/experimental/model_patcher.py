0043d0ee74617c515d3c46f36e7a35fef85beb05
Linkun Chen <github@lkchen.net>
2026-04-08 17:39:34 +0000

    revert unintended
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- tpu_inference/models/vllm/experimental/model_patcher.py
+++ tpu_inference/models/vllm/experimental/model_patcher.py
@@ -108,12 +108,7 @@ def patch_mm_model(
 
         # Start from 1 because 'model' is our root
         for name in module_names[1:-1]:
-            try:
-                cur_module = getattr(cur_module, name)
-            except AttributeError as e:
-                raise ValueError(
-                    f"{type(cur_module)} has attributes {dir(cur_module)}"
-                ) from e
+            cur_module = getattr(cur_module, name)
 
         target_module_name = module_names[-1]
         jitted_module = JittableModule(getattr(cur_module, target_module_name))
