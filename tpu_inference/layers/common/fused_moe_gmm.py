b1628048d155cb1410af88fa72b4fe40de419684
clee1994 <clemensschaefer1994@googlemail.com>
2026-04-09 18:45:26 +0000

    pre-com
    Signed-off-by: clee1994 <clemensschaefer1994@googlemail.com>

--- tpu_inference/layers/common/fused_moe_gmm.py
+++ tpu_inference/layers/common/fused_moe_gmm.py
@@ -21,8 +21,8 @@ from jax.sharding import Mesh, NamedSharding
 from jax.sharding import PartitionSpec as P
 
 import tpu_inference.envs as envs
-from tpu_inference.kernels.gather.ragged_gather import ragged_gather
 from tpu_inference.kernels.gather import gather_reduce as gather_reduce_sc
+from tpu_inference.kernels.gather.ragged_gather import ragged_gather
 from tpu_inference.kernels.megablox.gmm_v2 import gmm_v2
 from tpu_inference.layers.common.sharding import ShardingAxisName
 from tpu_inference.utils import get_mesh_shape_product
