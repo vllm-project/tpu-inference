fb8f81b72eb1a203b772dd1dcb7bd29470a86fa2
Linkun Chen <github@lkchen.net>
2026-04-17 20:59:35 +0000

    Update vllm_lkg.version to 04/13 noon, and fix missing Mxfp4MoEMethod
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- tpu_inference/layers/vllm/quantization/mxfp4.py
+++ tpu_inference/layers/vllm/quantization/mxfp4.py
@@ -31,8 +31,8 @@ from vllm.model_executor.layers.quantization import \
     register_quantization_config
 from vllm.model_executor.layers.quantization.base_config import \
     QuantizeMethodBase
-from vllm.model_executor.layers.quantization.mxfp4 import (Mxfp4Config,
-                                                           Mxfp4MoEMethod)
+from vllm.model_executor.layers.quantization.mxfp4 import (
+    GptOssMxfp4MoEMethod, Mxfp4Config)
 from vllm.model_executor.layers.quantization.utils.quant_utils import \
     is_layer_skipped
 
@@ -90,7 +90,7 @@ class VllmMxfp4Config(Mxfp4Config, VllmQuantConfig):
         return None
 
 
-class VllmMxfp4MoEMethod(Mxfp4MoEMethod):
+class VllmMxfp4MoEMethod(GptOssMxfp4MoEMethod):
 
     def __init__(
         self,
