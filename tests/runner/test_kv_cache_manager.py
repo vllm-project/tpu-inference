cc036e762572cf292633bf5782a29e88d6efaedf
Linkun Chen <github@lkchen.net>
2026-04-22 17:20:20 +0000

    [LKG] set mamba_block_size in test
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- tests/runner/test_kv_cache_manager.py
+++ tests/runner/test_kv_cache_manager.py
@@ -791,6 +791,7 @@ class TestKVCacheManager:
 
     def test_get_kv_cache_spec_hybrid_mamba_cache_config_updates(self):
         self.runner.cache_config.block_size = 1056
+        self.runner.cache_config.mamba_block_size = 1056
         self.runner.kv_cache_dtype = torch.float8_e4m3fn
         self.runner.cache_config.mamba_page_size_padded = 533504
 
