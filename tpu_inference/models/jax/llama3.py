02020861658344680c13ad7b36c8eaef08806cfe
wenxindongwork <wenxindong@google.com>
2026-04-22 21:25:35 +0000

    revert llama changes

--- tpu_inference/models/jax/llama3.py
+++ tpu_inference/models/jax/llama3.py
@@ -92,11 +92,11 @@ class LlamaAttention(nnx.Module):
 
     def __init__(self, config: LlamaConfig, dtype: jnp.dtype, rng: nnx.Rngs,
                  mesh: Mesh, kv_cache_dtype: str):
-        # set_default_rope_theta(config, default_theta=500000)
+        set_default_rope_theta(config, default_theta=500000)
         self.hidden_size = config.hidden_size
         self.num_heads = config.num_attention_heads
         self.num_kv_heads = config.num_key_value_heads
-        self.rope_theta = config.rope_theta
+        self.rope_theta = config.rope_parameters["rope_theta"]
         self.rope_scaling = getattr(config, "rope_scaling", None)
 
         self.head_dim_original = getattr(config, "head_dim",
