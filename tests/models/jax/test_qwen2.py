7ed9c61d98eccd6fc8040c37dc4274e3ebf1b382
patrick ji <patrickji2014@gmail.com>
2026-04-30 00:39:40 +0000

    fix test failure due to vllm upstream pr
    Signed-off-by: patrick ji <patrickji2014@gmail.com>

--- tests/models/jax/test_qwen2.py
+++ tests/models/jax/test_qwen2.py
@@ -166,7 +166,10 @@ class TestQwen2ForCausalLM:
 
         # Test model load
         with jax.set_mesh(mesh), set_current_vllm_config(mock_vllm_config):
-            loader = get_model_loader(LoadConfig(load_format="hf"))
+            loader = get_model_loader(
+                LoadConfig(
+                    load_format="hf",
+                    model_loader_extra_config={"enable_weights_track": False}))
             loader.load_weights(model, model_config)
 
         # Test model forward
