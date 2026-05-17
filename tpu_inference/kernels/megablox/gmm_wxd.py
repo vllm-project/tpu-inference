045db2775d247bc744a9598f34940b3c98d1ab8c
wenxindongwork <wenxindong@google.com>
2026-05-17 18:25:32 +0000

    triple buffer

--- tpu_inference/kernels/megablox/gmm_wxd.py
+++ tpu_inference/kernels/megablox/gmm_wxd.py
@@ -150,7 +150,7 @@ def kernel_outer(group_sizes_ref,
     grid = ( h // tile_h, num_gm, d // tile_d) 
     in_specs = [
         pl.BlockSpec(block_shape=(tile_b, tile_d), index_map=lambda  h_idx, gm_idx, d_idx: (gm_lhs_start_ref[gm_idx] // tile_b, d_idx)),
-        pl.BlockSpec(block_shape=(1, tile_d, tile_h), index_map=lambda  h_idx, gm_idx, d_idx: (gm_expert_ids_ref[gm_idx], d_idx, h_idx)),
+        pl.BlockSpec(block_shape=(1, tile_d, tile_h), index_map=lambda  h_idx, gm_idx, d_idx: (gm_expert_ids_ref[gm_idx], d_idx, h_idx),pipeline_mode=pl.Buffered(buffer_count=3)),
     ]
     out_specs = pl.BlockSpec(block_shape=(tile_b, tile_h), index_map=lambda  h_idx, gm_idx,  d_idx: (gm_lhs_start_ref[gm_idx] // tile_b, h_idx))
 
@@ -243,55 +243,3 @@ if __name__ == "__main__":
     for label, times in timings.items():
         avg = sum(times) / len(times)
         print(f"PASSED [{label}] avg {avg*1000:.2f} ms")
-
-
-    # v1. all tokens go through matmul for each export
-
-
-    # PASSED [even routing (64, 64)] in 0.1136634349822998
-    # PASSED [uneven routing (96, 32)] in 0.11358451843261719
-    # PASSED [uneven routing (32, 96)] in 0.10309863090515137
-    # PASSED [one empty expert (128, 0)] in 0.10653448104858398
-    # PASSED [one empty expert (0, 128)] in 0.1001133918762207
-    # PASSED [extreme skew (1, 127)] in 0.10481739044189453
-
-
-
-    # v2. use jax.lax.cond to skip matmul for non-overlapping experts. s
-
-    # PASSED [even routing (64, 64)] avg 111.47 ms
-    # PASSED [uneven routing (96, 32)] avg 110.56 ms
-    # PASSED [uneven routing (32, 96)] avg 111.72 ms
-    # PASSED [one empty expert (128, 0)] avg 109.14 ms
-    # PASSED [one empty expert (0, 128)] avg 110.48 ms
-    # PASSED [extreme skew (1, 127)] avg 127.90 ms
-
-
-    # v3. use gm
-
-    # --- Average over 10 runs ---
-    # PASSED [even routing (64, 64)] avg 238.79 ms
-    # PASSED [uneven routing (96, 32)] avg 205.71 ms
-    # PASSED [uneven routing (32, 96)] avg 200.84 ms
-    # PASSED [one empty expert (128, 0)] avg 204.95 ms
-    # PASSED [one empty expert (0, 128)] avg 197.88 ms
-    # PASSED [extreme skew (1, 127)] avg 198.32 ms
-
-    # v4 . jitted
-
-    # PASSED [even routing (64, 64)] avg 23.03 ms
-    # PASSED [uneven routing (96, 32)] avg 0.21 ms
-    # PASSED [uneven routing (32, 96)] avg 0.22 ms
-    # PASSED [one empty expert (128, 0)] avg 27.42 ms
-    # PASSED [one empty expert (0, 128)] avg 0.23 ms
-    # PASSED [extreme skew (1, 127)] avg 0.20 ms
-
-
-    # v5 emit pipeline
-
-    # PASSED [even routing (64, 64)] avg 0.27 ms
-    # PASSED [uneven routing (96, 32)] avg 0.26 ms
-    # PASSED [uneven routing (32, 96)] avg 0.27 ms
-    # PASSED [one empty expert (128, 0)] avg 0.26 ms
-    # PASSED [one empty expert (0, 128)] avg 0.28 ms
-    # PASSED [extreme skew (1, 127)] avg 0.26 ms
