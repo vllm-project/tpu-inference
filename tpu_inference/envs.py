58b3b91389878c3f16bd39258de9f08c9ef1e079
wenxindongwork <wenxindong@google.com>
2026-05-18 17:50:51 +0000

    wip
    Signed-off-by: wenxindongwork <wenxindong@google.com>

--- tpu_inference/envs.py
+++ tpu_inference/envs.py
@@ -55,7 +55,7 @@ if TYPE_CHECKING:
     MOE_APPROX_TOPK_RECALL_TARGET: float | None = None
     VLLM_TPU_PATCH_MM_EMBEDDINGS: bool = False
     ENABLE_RS_KERNEL: bool = False
-    NUM_PRECOMPILE_WORKERS: int = 8
+    NUM_PRECOMPILE_WORKERS: int = 1
     DP_SCHED_BATCH_PREFILL: bool = False
     DP_SCHED_BATCH_PREFILL_FLUSH_TIMEOUT_MS: int = 10000
 
@@ -354,7 +354,7 @@ environment_variables: dict[str, Callable[[], Any]] = {
     env_bool("ENABLE_RS_KERNEL", default=False),
     # Number of worker threads for parallel XLA precompilation.
     "NUM_PRECOMPILE_WORKERS":
-    lambda: int(os.getenv("NUM_PRECOMPILE_WORKERS") or "8"),
+    lambda: int(os.getenv("NUM_PRECOMPILE_WORKERS") or "1"),
     # DP scheudler: hold and batch incoming requests (prefills) to
     # cluster and dispatch prefills together.
     "DP_SCHED_BATCH_PREFILL":
