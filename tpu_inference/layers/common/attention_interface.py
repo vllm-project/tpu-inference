fcbf975b04ffeb53dca990b2ee4e79386521894a
patrick ji <patrickji2014@gmail.com>
2026-05-18 18:43:45 +0000

    update num_kv_pages_per_block
    Signed-off-by: patrick ji <patrickji2014@gmail.com>

--- tpu_inference/layers/common/attention_interface.py
+++ tpu_inference/layers/common/attention_interface.py
@@ -540,7 +540,7 @@ def mla_attention(
 
     def _mla_ragged_paged_attention(q, q_rope, k, k_rope, cache, *args):
         # TODO: use auto tuner to find the best block sizes.
-        num_kv_pages_per_block = (2, 1, 1)
+        num_kv_pages_per_block = (8, 1, 1)
         num_queries_per_block = (1, 16, 16)
         decode_batch_size = 8
 
