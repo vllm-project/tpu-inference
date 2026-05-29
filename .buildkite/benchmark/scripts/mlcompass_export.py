52ed04588e988c3c677f5901c06fd5364c3e3950
Orti Bazar <orti.bazar@gmail.com>
2026-05-29 14:13:43 -0700

    Test1

--- .buildkite/benchmark/scripts/mlcompass_export.py
+++ .buildkite/benchmark/scripts/mlcompass_export.py
@@ -1,1 +1,10 @@
+import sys
+
+# sys.argv[0] is always the script name itself
+print(f"Script name: {sys.argv[0]}")
+
+# Capture the rest of the arguments
+if len(sys.argv) > 1:
+    print(f"Arguments passed: {sys.argv[1:]}")
+
 print('MLCompass export file called')
