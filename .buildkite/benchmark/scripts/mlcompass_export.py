4a48b8ab4e243eef3ed8b0bea2025a21e00048ee
Orti Bazar <orti.bazar@gmail.com>
2026-05-29 14:59:25 -0700

    Add bigquery export logic

--- .buildkite/benchmark/scripts/mlcompass_export.py
+++ .buildkite/benchmark/scripts/mlcompass_export.py
@@ -1,4 +1,9 @@
+import json
+import os
 import sys
+import time
+import uuid
+from google.cloud import bigquery
 
 # sys.argv[0] is always the script name itself
 print(f"Script name: {sys.argv[0]}")
@@ -6,5 +11,29 @@ print(f"Script name: {sys.argv[0]}")
 # Capture the rest of the arguments
 if len(sys.argv) > 1:
     print(f"Arguments passed: {sys.argv[1:]}")
+
+row_id = uuid.uuid4().hex
+
+row = {
+    'entry_id': row_id,
+    'sponge_id': os.getenv('MLCOMPASS_SPONGE_ID'),
+    'test_name': 'orti-vllm-test',
+    'succeeded': False,
+    'metrics': json.dumps({}),
+    'link_map': json.dumps({}),
+    'exc_timestamp_millis': int(time.time() * 1000),
+    'client_info': {
+        'github_commit': 'commit_sha_core',
+        'commit_branch_name': 'commit_branch',
+    },
+    'env_commit_map': json.dumps({}),
+    'mlcompass_tracking_id': os.getenv('MLCOMPASS_TRACKING_ID', row_id),
+    'mlcopmass_execution_mode': os.getenv('MLCOPMASS_EXECUTION_MODE', 'oneshot'),
+}
+
+client = bigquery.Client(project='google.com:ml-compass-benchmarks')
+table_ref = client.dataset('benchmarks_dataset').table('benchmarks_dev')
+table_obj = client.get_table(table_ref)
+client.insert_rows(table_obj, [row])
 
 print('MLCompass export file called')
