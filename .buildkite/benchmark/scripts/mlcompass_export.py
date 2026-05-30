452bd77c7ba0814c799f9d7bbb4e0be561110723
Orti Bazar <orti.bazar@gmail.com>
2026-05-29 17:33:26 -0700

    checking gcp service accounts

--- .buildkite/benchmark/scripts/mlcompass_export.py
+++ .buildkite/benchmark/scripts/mlcompass_export.py
@@ -14,6 +14,8 @@ print(credentials.service_account_email)
 # sys.argv[0] is always the script name itself
 print(f"Script name: {sys.argv[0]}")
 
+# agent gcp project id cloud-ullm-inference-ci-cd
+
 # Capture the rest of the arguments
 if len(sys.argv) > 1:
     print(f"Arguments passed: {sys.argv[1:]}")
