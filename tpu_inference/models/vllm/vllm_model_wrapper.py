bb42fe9053e052ebe309f1bc57bd2882fe62fee1
Yiwei Wang <yiwei.dddd@gmail.com>
2026-06-02 11:05:35 -0700

    Fix Accuracy test failure for gemma-4-31B-it (#2766)
    Signed-off-by: Yiwei Wang <yiwei.dddd@gmail.com>

--- tpu_inference/models/vllm/vllm_model_wrapper.py
+++ tpu_inference/models/vllm/vllm_model_wrapper.py
@@ -508,10 +508,8 @@ class VllmModelWrapper:
                         "call_method": "embed_input_ids",
                         "call_args": call_args,
                         "call_kwargs": {
-                            "is_multimodal":
-                            torch_view(is_multimodal)
-                            if is_multimodal is not None else False,
-                        },
+                            "is_multimodal": torch_view(is_multimodal)
+                        } if is_multimodal is not None else {},
                     },
                     tie_weights=False,
                 )
