54bfcc465281b173e02b4281121e0e2c902b1ca5
alynie <alynie@google.com>
2026-06-29 21:37:17 +0000

    perf: restore dimension_semantics to batched RPA compiler inner loop

--- tpu_inference/kernels/experimental/batched_rpa/kernel.py
+++ tpu_inference/kernels/experimental/batched_rpa/kernel.py
@@ -421,6 +421,7 @@ def rpa_kernel(
                 kv_lens_ref=kv_lens_ref,
             ),
             grid=(safe_steps, ),
+            dimension_semantics=("arbitrary", ),
             in_specs=(q_alloc.spec, kv_cache_alloc.spec),
             out_specs=(o_alloc.spec, ),
         )
