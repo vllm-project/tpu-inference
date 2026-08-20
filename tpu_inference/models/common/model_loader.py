7839ba60183a21b86d942f8f49cdb3aca1ba7129
Andy Twigg <atwigg@google.com>
2026-08-20 21:27:24 +0000

    fix(model_loader): remove unsupported graph_updates kwarg from nnx.eval_shape

--- tpu_inference/models/common/model_loader.py
+++ tpu_inference/models/common/model_loader.py
@@ -274,7 +274,7 @@ def _get_nnx_model(
                 mesh,
                 apply_to_abstract_model=True)
         with jax.set_mesh(mesh):
-            model = nnx.eval_shape(abstract_model_fn, graph_updates=False)
+            model = nnx.eval_shape(abstract_model_fn)
         # Although the created model can already work, we still need to jit
         # the model creation again, otherwise the model forward will have
         # non-trivial overhead in PjitFunction.
