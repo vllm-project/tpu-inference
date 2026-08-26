88d663ac763486bd4692cce98d76810e1f00f3b4
Aman Seervi <amanseervi@google.com>
2026-08-26 07:23:12 +0000

    Fix remaining MagicMock input_batch max_decode_tokens in tests
    Signed-off-by: Aman Seervi <amanseervi@google.com>

--- tests/runner/test_persistent_batch_manager.py
+++ tests/runner/test_persistent_batch_manager.py
@@ -331,6 +331,7 @@ class TestPersistentBatchManager(unittest.TestCase):
         requests = {req_id: req_state}
 
         input_batch = MagicMock()
+        input_batch.max_decode_tokens = 1
         input_batch.req_id_to_index = {req_id: 0}
         input_batch.num_prompt_tokens = np.array([2], dtype=np.int32)
         input_batch.token_ids_cpu = np.zeros((1, 10), dtype=np.int32)
